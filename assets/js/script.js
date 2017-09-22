c522dcf339c15f541212c2fe21c0ef1a3c4ac1da
Raymond P. Florendo <darkraven@cock.li>
2017-09-22 19:18:12 +0800

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
 window.onload = function() { document.body.className = ''; }
 window.ontouchmove = function() { return false; }
 window.onorientationchange = function() { document.body.scrollTop = 0; }
-document.getElementById('header').body = 'Former I.T Personality';
